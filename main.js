b29e516733ea9fbe18a2481253cc1199a273c024
masashi2525 <noerunoe8@gmail.com>
2020-05-08 12:29:41 +0900

    おみくじ完成

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    const results = ['大吉', '中吉', '吉', '凶'];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];   
+  });
+}
